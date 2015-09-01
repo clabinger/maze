6f7bf31f1123d1e9a733d6e00ab0654749983426
Cooper Labinger <clabinger@gmail.com>
2015-08-31 20:50:41 -0400

    (Mostly) solved maze

--- maze.js
+++ maze.js
@@ -2,21 +2,34 @@ var maze = function(width, height, canvas){
 
 	// Initialize the structure
 
-	var cell_size = 10;
+	var c = canvas.getContext("2d");
+
+	var cell_size = 40;
 
 	var structure = [];
+	var visit_index = [];
 
 	for(var i = 0; i < width; i++){
 		structure[i] = [];
+		visit_index[i] = [];
 		for(var j = 0; j < height; j++){			
 			structure[i][j] = [1, 1, 1, 1]; // top, right, bottom, left
+			visit_index[i][j] = 0;
 		}
 	}
 
-	var draw_borders = function(i, j, walls, c){
+	var compare_cells = function(a, b){
+			return (a[0]===b[0] && a[1]===b[1]);
+	}
+
+	var draw_borders = function(cell, walls){
+		
+		var i = cell[0];
+		var j = cell[1];
+
 		var start_x = i * cell_size;
 		var start_y = j * cell_size;
-
+		
 		if(walls[0]){
 			c.moveTo(start_x, start_y);
 			c.lineTo(start_x + cell_size, start_y);
@@ -43,16 +56,32 @@ var maze = function(width, height, canvas){
 
 	}
 
+	var fill_cell = function(cell, color){
+		
+		var i = cell[0];
+		var j = cell[1];
+
+		var start_x = i * cell_size;
+		var start_y = j * cell_size;
+
+		c.fillStyle = color;
+
+		c.fillRect(start_x+2, start_y+2, cell_size-4, cell_size-4);
+
+	}
+
 	var display_maze = function(){
 
-		var c = canvas.getContext("2d");
 		c.fillStyle = "#000";
 
 		for(var i = 0; i < width; i++){
 			for(var j = 0; j < height; j++){			
-				draw_borders(i, j, structure[i][j], c);
+				draw_borders([i, j], structure[i][j]);
 			}
 		}
+
+		fill_cell(maze_start, 'blue');
+		fill_cell(maze_end, 'red');
 
 	};
 
@@ -96,6 +125,21 @@ var maze = function(width, height, canvas){
 
 		return neighbors;
 
+	}
+
+	var get_reachable_neighbors = function(pos){
+	
+		var neighbors = get_neighbors(pos);
+
+		var reachable_neighbors = [];
+
+		for(var i in neighbors){
+			if(structure[ pos[0] ][ pos[1] ][get_relation(pos, neighbors[i])[0]]===0 && visit_index[neighbors[i][0]][neighbors[i][1]]===0){
+				reachable_neighbors.push( neighbors[i] );
+			}
+		}
+
+		return reachable_neighbors;
 	}
 
 	var get_unvisited_neighbors = function(pos){
@@ -153,10 +197,65 @@ var maze = function(width, height, canvas){
 
 	}
 
-	// console.log(structure);
+	var maze_start, maze_end, maze_start_temp;
+
+	if(Math.floor(Math.random() * 2)===0){
+		maze_start = [0, Math.floor(Math.random() * height)];
+		maze_end = [width-1, Math.floor(Math.random() * height)];
+	}else{
+		maze_start = [Math.floor(Math.random() * width), 0];
+		maze_end = [Math.floor(Math.random() * width), height-1];
+	}
+
+	if(Math.floor(Math.random() * 2)===0){
+		maze_start_temp = maze_start;
+		maze_start = maze_end;
+		maze_end = maze_start_temp;
+	}
 
 	display_maze();
 
-	console.log(get_relation([0,0], [0,1]));
+	// Solve maze
+
+	pos = maze_start;
+	visited = [pos];
+
+
+	while(!compare_cells(pos, maze_end)){
+
+		var reachable_neighbors = get_reachable_neighbors(pos);
+
+		if(reachable_neighbors.length>0){
+			var target = reachable_neighbors[Math.floor(Math.random() * reachable_neighbors.length)];
+			
+			visit_index[target[0]][target[1]] = 1;
+
+			visited.push(target);
+
+			if(!compare_cells(target, maze_start) && !compare_cells(target, maze_end)){
+				fill_cell(target, 'green');
+			}
+
+			pos = target;
+		}else{
+
+			if(!compare_cells(pos, maze_start) && !compare_cells(pos, maze_end)){
+				fill_cell(pos, 'brown');
+			}
+			
+			if(visited.length>0){
+				pos = visited.pop();
+			}
+			
+		}
+
+	}
+
+
+
+
+	
+
+	
 
 }
